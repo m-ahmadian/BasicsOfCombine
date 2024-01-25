a1d0ed4cb2712637f4430536c4f998508f8706cb
m-ahmadian <mehrdad.ahmadian@gmail.com>
2024-01-24 22:50:59 -0500

    Add project files

---
+++ BasicsOfCombine/BasicsOfCombine/BasicsOfCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicsOfCombineApp.swift
+//  BasicsOfCombine
+//
+//  Created by Mehrdad Behrouz Ahmadian on 2024-01-24.
+//
+
+import SwiftUI
+
+@main
+struct BasicsOfCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
